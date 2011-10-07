ca0861939e8d4daf77fe726872c8b7a291253a5f
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-10-07 12:10:17 +0300

    Finished first splash screen, working on event of the day

--- gen/com/NativeSample/R.java
+++ gen/com/NativeSample/R.java
@@ -27,6 +27,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050001;
+        public static final int event_day_description=0x7f050003;
+        public static final int event_day_title=0x7f050002;
         public static final int hello=0x7f050000;
     }
     public static final class style {

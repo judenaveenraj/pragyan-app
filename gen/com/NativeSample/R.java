2000f618b525000f3421032d021f76f22d1b181a
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-10-07 14:06:07 +0300

    Completed title bar for event of day screen

--- gen/com/NativeSample/R.java
+++ gen/com/NativeSample/R.java
@@ -15,15 +15,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int splash_logo=0x7f020001;
+        public static final int dancecouple=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int splash_logo=0x7f020002;
+        public static final int title_border=0x7f020003;
     }
     public static final class id {
-        public static final int imageView1=0x7f070000;
+        public static final int igeView1=0x7f070000;
+        public static final int imageView1=0x7f070001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int eventday=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int splash=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050001;

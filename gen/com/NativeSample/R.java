80339d44611d56f1ea17d5f552bad9710974c167
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-10-09 00:16:34 +0300

    Done with eventofday and added orientation reactivity

--- gen/com/NativeSample/R.java
+++ gen/com/NativeSample/R.java
@@ -25,8 +25,9 @@ public final class R {
     }
     public static final class layout {
         public static final int eventday=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int splash=0x7f030002;
+        public static final int eventday_land=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050001;

ae01273c43717f02a82b6a157ccaa878b6c4872c
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-10-27 18:36:27 +0300

    Done with listviewer, fix response position main page

--- src/com/NativeSample/ShowListViewer.java
+++ src/com/NativeSample/ShowListViewer.java
@@ -5,22 +5,13 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
-public class ShowListViewer extends Activity {
+public class ShowListViewer extends ListActivity {
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.listviewer);
-	    // TODO Auto-generated method stub
-	}
-
-	public class TutListActivity extends ListActivity {
-	    private String strings[]={"Crack the Shell", "Digital Fortress", "Labyrinth"};
+	    private String strings[]={"Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth","Crack the Shell", "Digital Fortress", "Labyrinth",};
 		@Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-		    setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, strings));
+	        setContentView(R.layout.listviewer);
+		    setListAdapter(new ArrayAdapter(this, R.layout.list_item, strings));
 	    }
 	}
-}
